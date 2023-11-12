443c4441dc5f3eeb17dbcfc07af103fff2da6303
hamuham <uni.uni.yui@hotmail.co.jp>
2023-11-12 21:33:18 +0900

    StyledBadge

--- frontend/src/features/core/Core.tsx
+++ frontend/src/features/core/Core.tsx
@@ -7,36 +7,36 @@ import { AppDispatch } from "../../app/store";
 
 import { withStyles } from "@material-ui/core/styles";
 import {
-  Button,
-  Grid,
-  Avatar,
-  Badge,
-  CircularProgress,
+    Button,
+    Grid,
+    Avatar,
+    Badge,
+    CircularProgress,
 } from "@material-ui/core";
 
 import { MdAddAPhoto } from "react-icons/md";
 
 import {
-  editNickname,
-  selectProfile,
-  selectIsLoadingAuth,
-  setOpenSignIn,
-  resetOpenSignIn,
-  setOpenSignUp,
-  resetOpenSignUp,
-  setOpenProfile,
-  resetOpenProfile,
-  fetchAsyncGetMyProf,
-  fetchAsyncGetProfs,
+    editNickname,
+    selectProfile,
+    selectIsLoadingAuth,
+    setOpenSignIn,
+    resetOpenSignIn,
+    setOpenSignUp,
+    resetOpenSignUp,
+    setOpenProfile,
+    resetOpenProfile,
+    fetchAsyncGetMyProf,
+    fetchAsyncGetProfs,
 } from "../auth/authSlice";
 
 import {
-  selectPosts,
-  selectIsLoadingPost,
-  setOpenNewPost,
-  resetOpenNewPost,
-  fetchAsyncGetPosts,
-  fetchAsyncGetComments,
+    selectPosts,
+    selectIsLoadingPost,
+    setOpenNewPost,
+    resetOpenNewPost,
+    fetchAsyncGetPosts,
+    fetchAsyncGetComments,
 } from "../post/postSlice";
 
 import Post from "../post/Post";
@@ -44,32 +44,32 @@ import EditProfile from "./EditProfile";
 import NewPost from "./NewPost";
 
 const StyledBadge = withStyles((theme) => ({
-  badge: {
-    backgroundColor: "#44b700",
-    color: "#44b700",
-    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    "&::after": {
-      position: "absolute",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%",
-      borderRadius: "50%",
-      animation: "$ripple 1.2s infinite ease-in-out",
-      border: "1px solid currentColor",
-      content: '""',
+    badge: {
+        backgroundColor: "#44b700",
+        color: "#44b700",
+        boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+        "&::after": {
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            borderRadius: "50%",
+            animation: "$ripple 1.2s infinite ease-in-out",
+            border: "1px solid currentColor",
+            content: '""',
+        },
     },
-  },
-  "@keyframes ripple": {
-    "0%": {
-      transform: "scale(.8)",
-      opacity: 1,
+    "@keyframes ripple": {
+        "0%": {
+            transform: "scale(.8)",
+            opacity: 1,
+        },
+        "100%": {
+            transform: "scale(2.4)",
+            opacity: 0,
+        },
     },
-    "100%": {
-      transform: "scale(2.4)",
-      opacity: 0,
-    },
-  },
 }))(Badge);
 
 
@@ -84,7 +84,7 @@ const Core: React.FC = () => {
         <div>
             <Auth />
             <div className={styles.core_header}>
-            <h1 className={styles.core_title}>SNS clone</h1>
+                <h1 className={styles.core_title}>SNS clone</h1>
                 {profile?.nickName ? (
                     <>
                         <button
@@ -109,11 +109,47 @@ const Core: React.FC = () => {
                             >
                                 Logout
                             </Buttonon>
+                            <button
+                                className={styles.core_btnModal}
+                                    onClick={() => {
+                                    dispatch(setOpenProfile());
+                                    dispatch(resetOpenNewPost());
+                                }}
+                            >
+                                <StyledBadge
+                                    overlap="circular"
+                                    anchorOrigin={{
+                                        vertical: "bottom",
+                                        horizontal: "right",
+                                    }}
+                                    variant="dot"
+                                >
+                                    <Avatar alt="who?" src={profile.img} />{" "}
+                                </StyledBadge>
+                            </button>
                         </div>
                     </>
                 ) : (
-                    <div></div>
+                    <div>
+                        <Button
+                            onClick={() => {
+                                dispatch(setOpenSignIn());
+                                dispatch(resetOpenSignUp());
+                            }}
+                        >
+                            LogIn
+                        </Button>
+                        <Button
+                            onClick={() => {
+                                dispatch(setOpenSignUp());
+                                dispatch(resetOpenSignIn());
+                            }}
+                        >
+                            SignUp
+                        </Button>
+                    </div>
                 )}
+            </div>
         </div>
     )
 }
